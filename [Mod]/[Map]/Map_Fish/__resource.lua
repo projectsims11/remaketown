14a0302bfa1861de567b6c810ea858b71d7811f2
Server <project.sims11@gmail.com>
2025-02-19 22:47:12 +0700

    Map fish

---
+++ [Mod]/[Map]/Map_Fish/__resource.lua
@@ -0,0 +1,32 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map "yes"
+
+
+files {
+
+	"stream/*.ytd",
+	"stream/*.ydr",
+	"stream/*.ycd",
+	
+	
+	
+}
+
+
+
+
+
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/lsland_job.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/lsland_job2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/lsny_wareventforest.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pmap_coconut_tree.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pmap_fish_1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pmap_fish_ani.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pmap_fish_ani1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pmap_pool5.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pmap_sand.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sand.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sm_light_001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/peaches_logo1.ytyp'
