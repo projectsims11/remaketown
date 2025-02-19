56ac22ba45f36b226a6572c9055c1b9d2e81501e
Server <project.sims11@gmail.com>
2025-02-19 12:48:19 +0700

    smaller scoreboard add map

---
+++ [Mod]/[Map]/logue_garage_casino/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/ytd/*.ytd',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_lucky_wheel.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_main.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_tables.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_slots.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_04.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sx_base_casino_garage.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sx_casino_top_building.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sx_door_garage_casino_001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sx_parking.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_04.ytyp'
