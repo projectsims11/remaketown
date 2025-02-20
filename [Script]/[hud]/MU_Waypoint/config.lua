dae14b118545b1cb5503a75c304f361d93015d42
Server <project.sims11@gmail.com>
2025-02-20 22:00:31 +0700

    edit waypoint

--- [Script]/[hud]/MU_Waypoint/config.lua
+++ [Script]/[hud]/MU_Waypoint/config.lua
@@ -9,14 +9,13 @@ Config = {}
 
 mu = {
     id = 1,  -- https://docs.fivem.net/docs/game-references/markers/ รูปแบบmarkersที่ใช้งาน
-    scale = 20.0, -- การมองเห็น
-    rgb = {r = 255, g = 0, b = 0, a = 200},  --สีmarkersที่ใช้งาน
+    scale = 5.0, -- การมองเห็น
+    rgb = {r = 0, g = 143, b = 255, a = 200},  --สีmarkersที่ใช้งาน
     height = 130.0 --ความสูง ค่าเดิมดีอยู่แล้ว
 }
 
 Config.MU_Enabled_Disabled = {
     MU_TextureEnabled = true, -- true เปิดใช้ markers แบบlogo กำหนดเอง /-- false ปิดใช้ markers แบบlogo กำหนดเอง 
-
     textureDict = 'mu', --ชื่อ markers ต้องตรงกัน
     textureName = 'mu', --ชื่อ markers ต้องตรงกัน
     textureMarkerScale = 15.0, -- ระยะ markers
