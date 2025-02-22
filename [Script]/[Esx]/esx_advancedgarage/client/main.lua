573d91cb7d77f7fc1678ca70f2c17ba01b73bb40
Server <project.sims11@gmail.com>
2025-02-22 09:19:40 +0700

    Merge Ruby esx-advancegarage

--- [Script]/[Esx]/esx_advancedgarage/client/main.lua
+++ [Script]/[Esx]/esx_advancedgarage/client/main.lua
@@ -94,9 +94,12 @@ function ListOwnedCarsMenu()
 				local enginpersen = json.decode(v.health_vehicles).health_engine
                 local bodypersen = json.decode(v.health_vehicles).health_body
                 local fuelpersen = json.decode(v.health_vehicles).fuel
-                local fuel = tostring(math.ceil(GetVehicleFuelLevel(fuelpersen)))
+                local fuel = tostring(math.ceil(fuelpersen))
                 local engine = tostring(math.ceil(enginpersen))
                 local body = tostring(math.ceil(bodypersen))
+
+			
+
 
 				SendNUIMessage({
 					garage = 'car',
